8596543b19ad917b22f479373a57c9cfc7d3a1c1
Sneha Singhania <sneha.a@iiitb.org>
2018-06-21 23:01:15 +0530

    utils.py initial commit

---
+++ utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def batch_generator(X, y, batch_size):
+    """
+    Primitive batch generator 
+    """
+    size = X.shape[0]
+    print (size)
+    X_copy = X.copy()
+    y_copy = y.copy()
+    indices = np.arange(size)
+    np.random.shuffle(indices)
+    X_copy = X_copy[indices]
+    y_copy = y_copy[indices]
+    i = 0
+    while True:
+        if i + batch_size <= size:
+            yield X_copy[i:i + batch_size], y_copy[i:i + batch_size]
+            i += batch_size
+        else:
+            i = 0
+            indices = np.arange(size)
+            np.random.shuffle(indices)
+            X_copy = X_copy[indices]
+            y_copy = y_copy[indices]
+            continue
+
